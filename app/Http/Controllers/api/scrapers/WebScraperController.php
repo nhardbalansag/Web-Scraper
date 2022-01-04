1f63bf38661bb79438c1d26c967e3465965a9476
nhardbalansag <nhardbalansag@gmail.com>
2022-01-04 08:53:20 +0800

    intall Symfony\Component\Panther\Client

--- app/Http/Controllers/api/scrapers/WebScraperController.php
+++ app/Http/Controllers/api/scrapers/WebScraperController.php
@@ -5,15 +5,32 @@ namespace App\Http\Controllers\api\scrapers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Goutte\Client;
+use GuzzleHttp\Client as GuzzleClient;
+use Symfony\Component\HttpClient\HttpClient;
 
 class WebScraperController extends Controller
 {
     public function GetCollection(){
-        $client = new Client();
-        $url =  "https://rarity.tools/cryptopunks";
-        $page = $client->request('GET', $url);
+        $client = new Client(HttpClient::create(['timeout' => 10000]));
+        // $client = new Client(HttpClient::create(['timeout' => 60]));
+        // $url =  "https://rarity.tools/cryptopunks";
+        // $page = $client->request('GET', $url);
 
-        $collectionSize = $page->filter("div");
+        // $collectionSize = $page->filter("div");
+
+        // $collectionSize->each(function($item){
+        //     dump($item->filter('div')->text());
+        // });
+        $client->setServerParameter('accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9');
+        $client->setServerParameter('accept-encoding', 'gzip, deflate, br');
+        $client->setServerParameter('accept-language', 'en-GB,en-US;q=0.9,en;q=0.8');
+        $client->setServerParameter('upgrade-insecure-requests', '1');
+        $client->setServerParameter('user-agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36');
+        $client->setServerParameter('connection', 'keep-alive');
+
+        $crawler = $client->request('GET', 'https://rarity.tools/cryptopunks');
+
+        $collectionSize = $crawler->filter("div");
 
         $collectionSize->each(function($item){
             dump($item->filter('div')->text());
@@ -29,21 +46,21 @@ class WebScraperController extends Controller
         $properties = array();
 
         // get collection details
-        $collectionName = $page->filter("h1")->text();
-        $collectionSize = $page->filter("div");
-        $propertyCount = null;
-        $collectionItem = null;
+        // $collectionName = $page->filter("h1")->text();
+        // $collectionSize = $page->filter("div");
+        // $propertyCount = null;
+        // $collectionItem = null;
 
         array_push(
             $properties,
             array(
-                "property_name" => 'attributeCount',
+                "property_name" => 'test property name 1',
                 "value" => 2,
                 "scoreContribution" => 21,
                 "supply" => 345
             ),
             array(
-                "property_name" => 'attributeCount',
+                "property_name" => 'est property name 2',
                 "value" => 2,
                 "scoreContribution" => 21,
                 "supply" => 345
@@ -61,7 +78,7 @@ class WebScraperController extends Controller
         );
 
         $data = array(
-            "collectionName" => $collectionName,
+            "collectionName" => "test",
             "collectionSize" => "test",
             "propertyCount" => "test",
             "items" => $collections
