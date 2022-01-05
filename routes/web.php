839d93fbd3c5e1428dd2c2870c0c98ebf2c7c44c
nhardbalansag <nhardbalansag@gmail.com>
2022-01-05 08:38:20 +0800

    remove route in web

--- routes/web.php
+++ routes/web.php
@@ -7,4 +7,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test/{id}', [WebScraperController::class, 'GetCollection']);
+// Route::get('/test/{id}', [WebScraperController::class, 'GetCollection']);
