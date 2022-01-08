406aef2422f9bb1b2866385e81f2e0527d826343
nhardbalansag <nhardbalansag@gmail.com>
2022-01-09 02:37:54 +0800

    refactoring

--- app/Http/Controllers/api/scrapers/WebScraperController.php
+++ app/Http/Controllers/api/scrapers/WebScraperController.php
@@ -14,12 +14,26 @@ use Exception;
 class WebScraperController extends Controller
 {
     public function GetCollection($request){
+        // "https://projects.rarity.tools/static/config/cryptopunks.json" //for config
+        // $collection_api_url = 'https://api.opensea.io/api/v1/assets?asset_contract_address=0xb47e3cd837ddf8e4c57f05d70ab865de6e193bbb&limit=20&token_ids=8348';
+        // $collection_json_data = file_get_contents($collection_api_url);
+        // $collection_response_data = json_decode($collection_json_data);
 
-        $request_collection = 'cryptopunks';
+        $config_data = "https://projects.rarity.tools/static/config/" . $request . ".json";
+        $config_data_json_data = file_get_contents($config_data);
+        $config_data_response_data = json_decode($config_data_json_data);
 
-        $exist = DB::table('collection_models')
-                ->where('collectionName', $request)
-                ->first();
+        // dd($config_data_response_data);
+
+        $asset_contract_address = $config_data_response_data->contracts[0]->contract;
+        $api_opensea_io_url = "https://api.opensea.io/api/v1/assets?asset_contract_address=" . $asset_contract_address . "&limit=20";
+        $api_opensea_io_url_json_data = file_get_contents($api_opensea_io_url);
+        $api_opensea_io_url_response_data = json_decode($api_opensea_io_url_json_data);
+        dd($api_opensea_io_url_response_data);
+
+        // $request_collection = 'cryptopunks';
+
+        $exist = $this->CheckExist($request);
 
         if(!$exist){
 
@@ -60,7 +74,8 @@ class WebScraperController extends Controller
                     $CollectionItemModel = new CollectionItemModel;
                     $CollectionItemModel->collection_item_id = $item_ids[$i][0];
                     $CollectionItemModel->collection_id = $collection_id;
-                    $CollectionItemModel->score = $total_score_value; //pending
+                    $CollectionItemModel->score = $total_score_value;
+                    $CollectionItemModel->rank = ($i + 1);
                     $CollectionItemModel->save();
 
                     for($j = 0; $j < count($item_ids[$i]); $j++){
@@ -87,137 +102,28 @@ class WebScraperController extends Controller
                     $CollectionDataModel->collection_item_id = $CollectionItemModel->id;
                     $CollectionDataModel->save();
                 }
+
                 DB::commit();
-
-                DB::table('collection_item_models')
-                ->where('id', $CollectionItemModel->id)
-                ->update(
-                    [
-                        'score' => $total_score_value
-                    ]
-                );
-
-                $collections = array();
-                $properties = array();
-
-                $exist = DB::table('collection_models')
-                        ->where('collectionName', $request)
-                        ->first();
-
-                $collection_item = DB::table('collection_item_models')
-                                ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                                ->select('collection_item_models.*')
-                                ->where('collection_models.id', $exist->id)
-                                ->get();
-
-                foreach($collection_item as $index => $data_collection_item){
-                    $properties_models = DB::table('properties_models')
-                                ->join('collection_item_models', 'collection_item_models.id', '=', 'properties_models.collection_item_id')
-                                ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                                ->select('properties_models.*')
-                                ->where('collection_models.id', $exist->id)
-                                ->where('properties_models.collection_item_id', $data_collection_item->id)
-                                ->get();
-
-                    foreach($properties_models as $index => $data){
-                        array_push(
-                            $properties,
-                            array(
-                                "property_name" => $data->property_name,
-                                "value" => $data->value,
-                                "scoreContribution" => $data->scoreContribution,
-                                "supply" => $data->supply
-                            )
-                        );
-                    }
-
-                    array_push(
-                        $collections,
-                        array(
-                            "id" => $data->id,
-                            "score" => $data->scoreContribution,
-                            "rank" => "pending",
-                            "properties" => $properties
-                        )
-                    );
-
-                    unset($properties);
-                    $properties = array();
-                }
-
-                $data = array(
-                    "collectionName" => $exist->collectionName,
-                    "collectionSize" => $exist->collectionSize,
-                    "propertyCount" => $exist->propertyCount,
-                    "items" => $collections
-                );
+                $data = $this->GetData($request);
+                dd($data);
             }catch(Exception $err){
                 DB::rollback();
                 dd($err->getMessage());
             }
         }else{
-            $collections = array();
-            $properties = array();
-
-            $collection_item = DB::table('collection_item_models')
-                            ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                            ->select('collection_item_models.*')
-                            ->where('collection_models.id', $exist->id)
-                            ->get();
-
-            foreach($collection_item as $index => $data_collection_item){
-                $properties_models = DB::table('properties_models')
-                            ->join('collection_item_models', 'collection_item_models.id', '=', 'properties_models.collection_item_id')
-                            ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                            ->select('properties_models.*')
-                            ->where('collection_models.id', $exist->id)
-                            ->where('properties_models.collection_item_id', $data_collection_item->id)
-                            ->get();
-
-                foreach($properties_models as $index => $data){
-                    array_push(
-                        $properties,
-                        array(
-                            "property_name" => $data->property_name,
-                            "value" => $data->value,
-                            "scoreContribution" => $data->scoreContribution,
-                            "supply" => $data->supply
-                        )
-                    );
-                }
-
-                array_push(
-                    $collections,
-                    array(
-                        "id" => $data->id,
-                        "score" => $data->scoreContribution,
-                        "rank" => "pending",
-                        "properties" => $properties
-                    )
-                );
-
-                unset($properties);
-                $properties = array();
-            }
-
-            $data = array(
-                "collectionName" => $exist->collectionName,
-                "collectionSize" => $exist->collectionSize,
-                "propertyCount" => $exist->propertyCount,
-                "items" => $collections
-            );
+            $data = $this->GetData($request);
+            dd($data);
         }
     }
 
     public function GetCollectionAPI($request){
 
-        $exist = DB::table('collection_models')
-                ->where('collectionName', $request)
-                ->first();
+        $exist = $this->CheckExist($request);
 
         if(!$exist){
             DB::beginTransaction();
             try{
+
                 $collection_api_url = 'https://collections.rarity.tools/collectionDetails/' . $request;
                 $collection_json_data = file_get_contents($collection_api_url);
                 $collection_response_data = json_decode($collection_json_data);
@@ -266,6 +172,7 @@ class WebScraperController extends Controller
                     $CollectionItemModel = new CollectionItemModel;
                     $CollectionItemModel->collection_item_id = $item_ids[$i][0];
                     $CollectionItemModel->collection_id = $collection_id;
+                    $CollectionItemModel->rank = ($i + 1);
                     $CollectionItemModel->score = $total_score_value; // total score is the addition of all scores per traits
                     $CollectionItemModel->save();
                     for($j = 0; $j < count($item_ids[$i]); $j++){
@@ -290,126 +197,88 @@ class WebScraperController extends Controller
                     $CollectionDataModel->collection_item_id = $CollectionItemModel->id;
                     $CollectionDataModel->save();
                 }
+
                 DB::commit();
+                $data = $this->GetData($request);
 
-                $collections = array();
-                $properties = array();
-
-                $exist = DB::table('collection_models')
-                        ->where('collectionName', $request)
-                        ->first();
-
-                $collection_item = DB::table('collection_item_models')
-                                ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                                ->select('collection_item_models.*')
-                                ->where('collection_models.id', $exist->id)
-                                ->get();
-
-                foreach($collection_item as $index => $data_collection_item){
-                    $properties_models = DB::table('properties_models')
-                                ->join('collection_item_models', 'collection_item_models.id', '=', 'properties_models.collection_item_id')
-                                ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                                ->select('properties_models.*', 'collection_item_models.score', 'collection_item_models.collection_item_id')
-                                ->where('collection_models.id', $exist->id)
-                                ->where('properties_models.collection_item_id', $data_collection_item->id)
-                                ->get();
-
-                    foreach($properties_models as $index => $data){
-                        array_push(
-                            $properties,
-                            array(
-                                "property_name" => $data->property_name,
-                                "value" => $data->value,
-                                "scoreContribution" => $data->scoreContribution,
-                                "supply" => $data->supply
-                            )
-                        );
-                    }
-
-                    array_push(
-                        $collections,
-                        array(
-                            "id" => $data->collection_item_id,
-                            "score" => $data->score,
-                            "rank" => "pending",
-                            "properties" => $properties
-                        )
-                    );
-
-                    unset($properties);
-                    $properties = array();
-                }
-
-                $data = array(
-                    "collectionName" => $exist->collectionName,
-                    "collectionSize" => $exist->collectionSize,
-                    "propertyCount" => $exist->propertyCount,
-                    "items" => $collections
-                );
-
-                return response()->json($data,  200, [], JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
             }catch(Exception $err){
                 DB::rollback();
                 return response()->json(
                     [
                         "message" => $err->getMessage(),
                         "status" => "Not Found"
-                    ],  500, [], JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+                    ],  500, [], JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT
+                );
             }
         }else{
-            $collections = array();
-            $properties = array();
+            $data = $this->GetData($request);
+        }
+        return response()->json($data,  200, [], JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+    }
 
-            $collection_item = DB::table('collection_item_models')
-                            ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                            ->select('collection_item_models.*')
-                            ->where('collection_models.id', $exist->id)
-                            ->get();
+    public function GetData($request){
+        $collections = array();
+        $properties = array();
 
-            foreach($collection_item as $index => $data_collection_item){
-                $properties_models = DB::table('properties_models')
-                            ->join('collection_item_models', 'collection_item_models.id', '=', 'properties_models.collection_item_id')
-                            ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
-                            ->select('properties_models.*', 'collection_item_models.score', 'collection_item_models.collection_item_id')
-                            ->where('collection_models.id', $exist->id)
-                            ->where('properties_models.collection_item_id', $data_collection_item->id)
-                            ->get();
+        $exist = $this->CheckExist($request);
 
-                foreach($properties_models as $index => $data){
-                    array_push(
-                        $properties,
-                        array(
-                            "property_name" => $data->property_name,
-                            "value" => $data->value,
-                            "scoreContribution" => $data->scoreContribution,
-                            "supply" => $data->supply
-                        )
-                    );
-                }
+        $collection_item = DB::table('collection_item_models')
+                        ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
+                        ->select('collection_item_models.*')
+                        ->where('collection_models.id', $exist->id)
+                        ->get();
 
+        foreach($collection_item as $index => $data_collection_item){
+            $properties_models = DB::table('properties_models')
+                        ->join('collection_item_models', 'collection_item_models.id', '=', 'properties_models.collection_item_id')
+                        ->join('collection_models', 'collection_models.id', '=', 'collection_item_models.collection_id')
+                        ->select('properties_models.*', 'collection_item_models.score', 'collection_item_models.rank' , 'collection_item_models.collection_item_id')
+                        ->where('collection_models.id', $exist->id)
+                        ->where('properties_models.collection_item_id', $data_collection_item->id)
+                        ->get();
+
+            foreach($properties_models as $index => $data){
                 array_push(
-                    $collections,
+                    $properties,
                     array(
-                        "id" => $data->collection_item_id,
-                        "score" => $data->score,
-                        "rank" => "pending",
-                        "properties" => $properties
+                        "property_name" => $data->property_name,
+                        "value" => $data->value,
+                        "scoreContribution" => $data->scoreContribution,
+                        "supply" => $data->supply
                     )
                 );
-
-                unset($properties);
-                $properties = array();
             }
 
-            $data = array(
-                "collectionName" => $exist->collectionName,
-                "collectionSize" => $exist->collectionSize,
-                "propertyCount" => $exist->propertyCount,
-                "items" => $collections
+            array_push(
+                $collections,
+                array(
+                    "id" => $data->collection_item_id,
+                    "score" => $data->score,
+                    "rank" => "pending",
+                    "properties" => $properties
+                )
             );
 
-            return response()->json($data,  200, [], JSON_UNESCAPED_SLASHES|JSON_PRETTY_PRINT);
+            unset($properties);
+            $properties = array();
         }
+
+        $data = array(
+            "collectionName" => $exist->collectionName,
+            "collectionSize" => $exist->collectionSize,
+            "propertyCount" => $exist->propertyCount,
+            "items" => $collections
+        );
+
+        return $data;
+    }
+
+
+    public function CheckExist($request){
+        $data = DB::table('collection_models')
+                ->where('collectionName', $request)
+                ->first();
+
+        return $data;
     }
 }
-
